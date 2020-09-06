9ab80af19d3b8f7e9daca0b853deeff078cae766
mcp <mcp@mcps-MacBook-Pro-2.local>
2020-09-06 14:55:06 -0600

    okay, let's grab the config file and shove it in

--- src/l4d2/start-l4d2.sh
+++ src/l4d2/start-l4d2.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 steamcmd +runscript /opt/l4d2/l4d2-steamcmd-script;
+wget https://raw.githubusercontent.com/mcpdude/l4d2-server/master/src/l4d2/addonconfig.cfg;
+mv addonconfig.cfg /opt/l4d2/game/left4dead2/cfg;
 /opt/l4d2/game/srcds_run -ip 0.0.0.0 -port 27015 -secure +map c5m1_waterfront;
